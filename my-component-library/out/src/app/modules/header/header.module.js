5b7b7fe424b4795b8ebdfaa9a66fd21fd8fddda2
anilpavuluri441 <anil1964000@gmail.com>
2018-04-24 09:43:46 +0530

    add my files

---
+++ my-component-library/out/src/app/modules/header/header.module.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+export class HeaderModule {
+}
+HeaderModule.decorators = [
+    { type: NgModule, args: [{
+                imports: [
+                    CommonModule
+                ],
+                declarations: [
+                    HeaderComponent
+                ],
+                exports: [
+                    HeaderComponent
+                ]
+            },] },
+];
+function HeaderModule_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    HeaderModule.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    HeaderModule.ctorParameters;
+}
+//# sourceMappingURL=header.module.js.map
